9c68288c8044bfb3b80282002f4da24a46cd9690
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-11 14:18:56 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController .cs
@@ -0,0 +1,35 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            using var ctx = new PizzaContext();
+
+            var pizzas = ctx.Pizzas.ToArray();
+
+            return View(pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+            using var ctx = new PizzaContext();
+
+            var pizza = ctx.Pizzas.Find(id);
+
+            if (pizza is null)
+            {
+                return NotFound($"L'id {id} della pizza non è stato trovato");
+            }
+
+            return View(pizza);
+        }
+
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PizzaDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public void Seed()
+        {
+
+            if (!Pizzas.Any())
+            {
+                var seed = new Pizza[]
+                {
+            new Pizza
+            {
+                Name = "Pizza Margherita",
+                Description = "Pizza classica dal gusto inconfondibile.",
+                Image = "img/margherita-50kalo.jpg",
+                Price = 4,
+            },
+            new Pizza
+            {
+                Name = "Americana",
+                Description = "Pizza che De Sica definirebbe con: 'Che è sta cafonata'.",
+                Image = "img/americana.jpg",
+                Price = 5,
+            },
+            new Pizza
+            {
+                Name = "Diavola",
+                Description = "Pizza leggera ma quel pizzico in più.",
+                Image = "img/diavola.jpg",
+                Price = 4.5,
+            },
+            new Pizza
+            {
+                Name = "Capricciosa",
+                Description = "Pizza amata dal ristoratore. Gli permette di liberarsi dei condimenti",
+                Image = "img/Capricciosa.jpg",
+                Price = 6.5,
+            }
+            };
+
+                Pizzas.AddRange(seed);
+
+                SaveChanges();
+            }
+        }
+    }
+}
+
