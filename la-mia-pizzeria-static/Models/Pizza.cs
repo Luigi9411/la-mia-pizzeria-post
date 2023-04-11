ec60f746870a70f00acb719ca5c55c5a3c6cdbac
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-11 18:46:48 +0200

    final

--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -7,14 +7,14 @@ namespace la_mia_pizzeria_static.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Aggiungi il nome")]
-        public string? Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Aggiungi la descrizione")]
         [StringLength(200, MinimumLength = 5, ErrorMessage = "Deve contenere almeno cinque parole")]
-        public string? Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Aggiungi l'immagine")]
-        public string? Image {get; set; }
+        public string? Image {get; set;}
 
         [Required(ErrorMessage = "Aggiungi il prezzo")]
         [Range(3, double.MaxValue)]
